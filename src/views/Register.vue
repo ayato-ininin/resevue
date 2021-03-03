823445ac121d3bf136d292fe2a1944a7e35cf590
ayato-ininin <sarai.a.1203408@gmail.com>
2021-03-03 16:04:14 +0900

    add

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,24 @@
 <template>
   <div id="app">
     <HeaderAuth2 />
+    
+      <div class="card">
+      <div class="banner">
+      <p class="title">Registration</p>
+      </div>
+      <div class="form">
+        <div class="icon">
+        <font-awesome-icon class="img" icon="user" /><input type="text" placeholder="Username">
+        </div>
+        <div class="icon">
+        <font-awesome-icon class="img" icon="envelope" /><input type="text" placeholder="Email">
+        </div>
+        <div class="icon">
+       <font-awesome-icon class="img" icon="lock" /> <input type="text" placeholder="Password">
+       </div>
+        <button>登録</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,5 +33,63 @@ export default{
 </script>
 
 <style scoped>
+.card{
+  background-color: #ffffff;
+  width: 400px;
+  height: 280px;
+  font-size: 1.4rem;
+  text-align: center;
+  margin: 0 auto;
+  margin-top:170px;
+  border: 5px;
+  border-radius: 5px;
+  box-shadow: 3px 3px 3px rgba(0,0,0,0.3);
+}
+button{
+  background-color: #305DFF;
+  color: #ffffff;
+  border:0px none;
+  cursor: pointer;
+  width: 55px;
+  height: 30px;
+  border-radius: 6px;
+ margin-top: 15px;
+}
+input{
+ border:0;
+ border-bottom: 1px solid ;
+width: 70%;
+height: 30px;
+}
+.banner{
+  width: 400px;
+  height: 55px;
+  background-color:#305DFF ;
+  display: block;
+  text-align: left;
+  line-height: 55px;
+  border-radius: 5px 5px 0 0;
+}
+.title{
+  color: #ffffff;
+  font-size: 1rem;
+  font-weight: bold;
+  margin-left: 20px;
+}
+.form{
+  align-items: center;
+ 
+}
+.icon{
+  display: flex;
+  justify-content:center;
+  margin-top: 15px;
+}
+.img{
+  width: 25px;
+  height: 25px;
+  margin-top: 8px;
+  margin-right: 15px;
 
+}
 </style>
