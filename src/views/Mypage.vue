0079778d1f9d337bc665dfa09dc366f4e5e7999e
ayato-ininin <sarai.a.1203408@gmail.com>
2021-05-23 15:01:32 +0900

    add

--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -174,11 +174,8 @@ export default{
      axios.delete("https://powerful-hollows-86374.herokuapp.com/api/reservations?id=" +reserve[1].id)
      .then((response)=>{
        console.log(response);
-       this.$router.replace("/");
-      //  this.$router.go({
-      //    path:this.$router.currentRoute.path,
-      //    force:true,
-      //  });
+       router.replace("/");
+     
      });
    },
    
